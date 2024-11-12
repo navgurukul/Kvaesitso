db0206a227d24357156ee93859249885fcde075b
shaina <shainakhatun21@navgurukul.org>
2024-11-12 12:40:08 +0530

    Second screen behaviour issue

--- app/ui/src/main/java/de/mm20/launcher2/ui/launcher/search/apps/AppResults.kt
+++ app/ui/src/main/java/de/mm20/launcher2/ui/launcher/search/apps/AppResults.kt
@@ -2,12 +2,8 @@ package de.mm20.launcher2.ui.launcher.search.apps
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Lock
@@ -15,16 +11,8 @@ import androidx.compose.material.icons.rounded.LockOpen
 import androidx.compose.material.icons.rounded.Person
 import androidx.compose.material.icons.rounded.Work
 import androidx.compose.material.icons.rounded.WorkOff
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.FilledTonalButton
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.LeadingIconTab
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.PrimaryScrollableTabRow
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -51,154 +39,143 @@ fun LazyListScope.AppResults(
     columns: Int,
     reverse: Boolean,
 ) {
-
-    GridResults(
-        key = "apps",
-        items = apps,
-        before = if (profiles.size > 1) {
-            {
-                Column(
-                    verticalArrangement = if (reverse) Arrangement.BottomReversed else Arrangement.Top,
-                ) {
-                    PrimaryScrollableTabRow(
-                        selectedTabIndex = selectedProfileIndex,
-                        containerColor = Color.Transparent,
-                        edgePadding = 16.dp,
-                        divider = {}
+    item {
+        this@AppResults.GridResults(
+            key = "apps",
+            items = apps,
+            before = if (profiles.size > 1) {
+                {
+                    Column(
+                        // Ensure the column aligns filters at the top
+                        verticalArrangement = Arrangement.Top,
                     ) {
-                        for ((i, profile) in profiles.withIndex()) {
-                            LeadingIconTab(
-                                selected = selectedProfileIndex == profiles.indexOf(profile),
-                                text = {
-                                    Text(
-                                        when (profile.type) {
-                                            Profile.Type.Personal -> stringResource(R.string.apps_profile_main)
-                                            Profile.Type.Work -> stringResource(R.string.apps_profile_work)
-                                            Profile.Type.Private -> stringResource(R.string.apps_profile_private)
-                                        }
-                                    )
-                                },
-                                icon = {
-                                    when (profile.type) {
-                                        Profile.Type.Personal -> Icon(
-                                            Icons.Rounded.Person,
-                                            contentDescription = null
-                                        )
-
-                                        Profile.Type.Work -> Icon(
-                                            Icons.Rounded.Work,
-                                            contentDescription = null
-                                        )
-
-                                        Profile.Type.Private -> Icon(
-                                            Icons.Rounded.PrivateSpace,
-                                            contentDescription = null
-                                        )
-                                    }
-                                },
-                                onClick = {
-                                    onProfileSelected(i)
-                                }
-                            )
-                        }
-                    }
-                    HorizontalDivider()
-
-                    val profileType = profiles[selectedProfileIndex].type
-                    if (profileType != Profile.Type.Personal) {
-                        if (isProfileLocked) {
-                            Column(
-                                modifier = Modifier
-                                    .padding(12.dp)
-                                    .fillMaxWidth()
-                                    .border(1.dp, MaterialTheme.colorScheme.outlineVariant, MaterialTheme.shapes.small)
-                                    .background(MaterialTheme.colorScheme.surfaceContainer, MaterialTheme.shapes.small)
-                                    .padding(vertical = 64.dp),
-                                verticalArrangement = Arrangement.Center,
-                                horizontalAlignment = Alignment.CenterHorizontally,
-                            ) {
-                                Icon(
-                                    if (profileType == Profile.Type.Work) Icons.Rounded.WorkOff else Icons.Rounded.Lock,
-                                    contentDescription = null,
-                                    modifier = Modifier.size(48.dp),
-                                    tint = MaterialTheme.colorScheme.secondary,
-                                )
-                                Text(
-                                    stringResource(
-                                        if (profileType == Profile.Type.Work) R.string.profile_work_profile_state_locked
-                                        else R.string.profile_private_profile_state_locked
-                                    ),
-                                    modifier = Modifier.padding(top = 8.dp),
-                                    color = MaterialTheme.colorScheme.secondary,
-                                    style = MaterialTheme.typography.titleSmall,
-                                )
-                                if (showProfileLockControls) {
-                                    Button(
-                                        modifier = Modifier.padding(top = 32.dp),
-                                        onClick = {
-                                            onProfileLockChange?.invoke(
-                                                profiles[selectedProfileIndex],
-                                                false
-                                            )
-                                        },
-                                        contentPadding = ButtonDefaults.TextButtonWithIconContentPadding,
-                                    ) {
-                                        Icon(
-                                            if (profileType == Profile.Type.Work) Icons.Rounded.Work else Icons.Rounded.LockOpen,
-                                            contentDescription = null,
-                                            modifier = Modifier
-                                                .padding(end = ButtonDefaults.IconSpacing)
-                                                .size(ButtonDefaults.IconSize)
-                                        )
+                        PrimaryScrollableTabRow(
+                            selectedTabIndex = selectedProfileIndex,
+                            containerColor = Color.Transparent,
+                            edgePadding = 16.dp,
+                            divider = {}
+                        ) {
+                            for ((i, profile) in profiles.withIndex()) {
+                                LeadingIconTab(
+                                    selected = selectedProfileIndex == i,
+                                    text = {
                                         Text(
-                                            stringResource(
-                                                if (profileType == Profile.Type.Work) R.string.profile_work_profile_action_unlock
-                                                else R.string.profile_private_profile_action_unlock
-                                            )
+                                            when (profile.type) {
+                                                Profile.Type.Personal -> stringResource(R.string.apps_profile_main)
+                                                Profile.Type.Work -> stringResource(R.string.apps_profile_work)
+                                                Profile.Type.Private -> stringResource(R.string.apps_profile_private)
+                                            }
                                         )
+                                    },
+                                    icon = {
+                                        when (profile.type) {
+                                            Profile.Type.Personal -> Icon(Icons.Rounded.Person, contentDescription = null)
+                                            Profile.Type.Work -> Icon(Icons.Rounded.Work, contentDescription = null)
+                                            Profile.Type.Private -> Icon(Icons.Rounded.PrivateSpace, contentDescription = null)
+                                        }
+                                    },
+                                    onClick = { onProfileSelected(i) }
+                                )
+                            }
+                        }
+                        HorizontalDivider()
+
+                        val profileType = profiles[selectedProfileIndex].type
+                        if (profileType != Profile.Type.Personal) {
+                            if (isProfileLocked) {
+                                Column(
+                                    modifier = Modifier
+                                        .padding(12.dp)
+                                        .fillMaxWidth()
+                                        .border(1.dp, MaterialTheme.colorScheme.outlineVariant, MaterialTheme.shapes.small)
+                                        .background(MaterialTheme.colorScheme.surfaceContainer, MaterialTheme.shapes.small)
+                                        .padding(vertical = 64.dp),
+                                    verticalArrangement = Arrangement.Center,
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                ) {
+                                    Icon(
+                                        if (profileType == Profile.Type.Work) Icons.Rounded.WorkOff else Icons.Rounded.Lock,
+                                        contentDescription = null,
+                                        modifier = Modifier.size(48.dp),
+                                        tint = MaterialTheme.colorScheme.secondary,
+                                    )
+                                    Text(
+                                        stringResource(
+                                            if (profileType == Profile.Type.Work) R.string.profile_work_profile_state_locked
+                                            else R.string.profile_private_profile_state_locked
+                                        ),
+                                        modifier = Modifier.padding(top = 8.dp),
+                                        color = MaterialTheme.colorScheme.secondary,
+                                        style = MaterialTheme.typography.titleSmall,
+                                    )
+                                    if (showProfileLockControls) {
+                                        Button(
+                                            modifier = Modifier.padding(top = 32.dp),
+                                            onClick = {
+                                                onProfileLockChange?.invoke(
+                                                    profiles[selectedProfileIndex],
+                                                    false
+                                                )
+                                            },
+                                            contentPadding = ButtonDefaults.TextButtonWithIconContentPadding,
+                                        ) {
+                                            Icon(
+                                                if (profileType == Profile.Type.Work) Icons.Rounded.Work else Icons.Rounded.LockOpen,
+                                                contentDescription = null,
+                                                modifier = Modifier
+                                                    .padding(end = ButtonDefaults.IconSpacing)
+                                                    .size(ButtonDefaults.IconSize)
+                                            )
+                                            Text(
+                                                stringResource(
+                                                    if (profileType == Profile.Type.Work) R.string.profile_work_profile_action_unlock
+                                                    else R.string.profile_private_profile_action_unlock
+                                                )
+                                            )
+                                        }
                                     }
                                 }
-                            }
-                        } else if (showProfileLockControls) {
-                            FilledTonalButton(
-                                modifier = Modifier
-                                    .padding(12.dp)
-                                    .fillMaxWidth(),
-                                onClick = {
-                                    onProfileLockChange?.invoke(
-                                        profiles[selectedProfileIndex],
-                                        true
-                                    )
-                                },
-                                contentPadding = ButtonDefaults.TextButtonWithIconContentPadding,
-                            ) {
-                                Icon(
-                                    if (profileType == Profile.Type.Work) Icons.Rounded.WorkOff else Icons.Rounded.Lock,
-                                    contentDescription = null,
+                            } else if (showProfileLockControls) {
+                                FilledTonalButton(
                                     modifier = Modifier
-                                        .padding(end = ButtonDefaults.IconSpacing)
-                                        .size(ButtonDefaults.IconSize)
-                                )
-                                Text(
-                                    stringResource(
-                                        if (profileType == Profile.Type.Work) R.string.profile_work_profile_action_lock
-                                        else R.string.profile_private_profile_action_lock
+                                        .padding(12.dp)
+                                        .fillMaxWidth(),
+                                    onClick = {
+                                        onProfileLockChange?.invoke(
+                                            profiles[selectedProfileIndex],
+                                            true
+                                        )
+                                    },
+                                    contentPadding = ButtonDefaults.TextButtonWithIconContentPadding,
+                                ) {
+                                    Icon(
+                                        if (profileType == Profile.Type.Work) Icons.Rounded.WorkOff else Icons.Rounded.Lock,
+                                        contentDescription = null,
+                                        modifier = Modifier
+                                            .padding(end = ButtonDefaults.IconSpacing)
+                                            .size(ButtonDefaults.IconSize)
                                     )
-                                )
+                                    Text(
+                                        stringResource(
+                                            if (profileType == Profile.Type.Work) R.string.profile_work_profile_action_lock
+                                            else R.string.profile_private_profile_action_lock
+                                        )
+                                    )
+                                }
                             }
                         }
                     }
                 }
-            }
-        } else null,
-        itemContent = {
-            GridItem(
-                item = it,
-                showLabels = LocalGridSettings.current.showLabels,
-                highlight = it.key == highlightedItem?.key
-            )
-        },
-        reverse = reverse,
-        columns = columns,
-    )
+            } else null,
+            itemContent = {
+                GridItem(
+                    item = it,
+                    showLabels = LocalGridSettings.current.showLabels,
+                    highlight = it.key == highlightedItem?.key
+                )
+            },
+            reverse = reverse,
+            columns = columns,
+        )
+    }
 }
