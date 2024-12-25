f6f5b2b589bc2dcea2bd4b2aed353333ac0eea74
Aayushi <aayushitiwari21@navgurukul.org>
2024-12-25 12:01:28 +0530

    Added menu in keyboard

--- app/ui/src/main/java/de/mm20/launcher2/ui/keyboard/QwertyKeyboard.kt
+++ app/ui/src/main/java/de/mm20/launcher2/ui/keyboard/QwertyKeyboard.kt
@@ -1,5 +1,7 @@
 package de.mm20.launcher2.ui.keyboard
 
+import android.content.Intent
+import android.provider.Settings
 import androidx.compose.animation.graphics.res.animatedVectorResource
 import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
 import androidx.compose.animation.graphics.vector.AnimatedImageVector
@@ -9,27 +11,33 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Edit
+import androidx.compose.material.icons.rounded.HelpOutline
 import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.material.icons.rounded.Wallpaper
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import de.mm20.launcher2.ktx.tryStartActivity
 import de.mm20.launcher2.ui.R
+import de.mm20.launcher2.ui.launcher.LauncherScaffoldVM
 import de.mm20.launcher2.ui.launcher.search.SearchVM
+import de.mm20.launcher2.ui.settings.SettingsActivity
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @Composable
@@ -79,7 +87,10 @@ fun QwertyKeyboard(
             KeyboardRowWithBackspace(
                 letters = listOf('z', 'x', 'c', 'v', 'b', 'n', 'm'),
                 onKeyPress = onKeyPress,
-                modifier = Modifier.fillMaxWidth(0.8f)
+                modifier = Modifier.fillMaxWidth(0.8f),
+                searchVM = searchVM,
+                searchBarValue = searchVM.searchQuery.value,
+                onInputClear = { searchVM.searchQuery.value = "" }
             )
         }
     }
@@ -99,13 +110,15 @@ fun KeyboardRow(letters: List<Char>, onKeyPress: (String) -> Unit, modifier: Mod
 }
 
 @Composable
-fun KeyboardRowWithBackspace(letters: List<Char>,onKeyPress: (String) -> Unit, modifier: Modifier) {
+fun KeyboardRowWithBackspace(letters: List<Char>,onKeyPress: (String) -> Unit, modifier: Modifier, searchVM: SearchVM,
+                             searchBarValue: String,
+                             onInputClear: () -> Unit) {
     Row(
         modifier = modifier
             .padding( vertical = 4.dp, horizontal = 4.dp),
         horizontalArrangement = Arrangement.spacedBy(3.dp),
     ) {
-        MenuKey(onKeyPress = onKeyPress, modifier = Modifier.weight(1f), SearchVM())
+        MenuKey(onKeyPress = onKeyPress, modifier = Modifier.weight(1f), searchVM, searchBarValue, onInputClear)
         letters.forEach { letter ->
             KeyboardKey(letter = letter, onKeyPress = onKeyPress, enabled = true, modifier = Modifier.weight(1f))
         }
@@ -155,18 +168,24 @@ fun BackspaceKey(onKeyPress: (String) -> Unit, modifier: Modifier) {
 
 
 @Composable
-fun MenuKey(onKeyPress: (String) -> Unit, modifier: Modifier, searchVM: SearchVM) {
+fun MenuKey(
+    onKeyPress: (String) -> Unit,
+    modifier: Modifier,
+    searchVM: SearchVM,
+    searchBarValue: String,
+    onInputClear: () -> Unit
+) {
+    val context = LocalContext.current
+    var showOverflowMenu by remember { mutableStateOf(false) }
     val rightIcon = AnimatedImageVector.animatedVectorResource(R.drawable.anim_ic_menu_clear)
+    val launcherVM: LauncherScaffoldVM = viewModel()
+
     Box(
         contentAlignment = Alignment.Center,
         modifier = modifier
             .size(40.dp)
-            .clickable {
-
-            }
+            .clickable { showOverflowMenu = true }
             .padding(1.dp)
-            //.background(Color., RoundedCornerShape(8.dp))
-
     ) {
         Icon(
             painter = rememberAnimatedVectorPainter(
@@ -177,5 +196,61 @@ fun MenuKey(onKeyPress: (String) -> Unit, modifier: Modifier, searchVM: SearchVM
             tint = Color.White
         )
     }
+
+    DropdownMenu(expanded = showOverflowMenu, onDismissRequest = { showOverflowMenu = false }) {
+        DropdownMenuItem(
+            onClick = {
+                context.startActivity(
+                    Intent.createChooser(
+                        Intent(Intent.ACTION_SET_WALLPAPER),
+                        null
+                    )
+                )
+                showOverflowMenu = false
+            },
+            text = {
+                Text(stringResource(R.string.wallpaper))
+            },
+            leadingIcon = {
+                Icon(imageVector = Icons.Rounded.Wallpaper, contentDescription = null)
+            }
+        )
+        DropdownMenuItem(
+            onClick = {
+                launcherVM.setWidgetEditMode(editMode = true)
+                showOverflowMenu = false
+            },
+            text = {
+                Text(stringResource(R.string.menu_edit_widgets))
+            },
+            leadingIcon = {
+                Icon(imageVector = Icons.Rounded.Edit, contentDescription = null)
+            }
+        )
+        DropdownMenuItem(
+            onClick = {
+                context.startActivity(Intent(context, SettingsActivity::class.java))
+                showOverflowMenu = false
+            },
+            text = {
+                Text(stringResource(R.string.settings))
+            },
+            leadingIcon = {
+                Icon(imageVector = Icons.Rounded.Settings, contentDescription = null)
+            }
+        )
+        DropdownMenuItem(
+            text = {
+                Text(stringResource(R.string.set_as_default_laucher))
+            },
+            onClick = {
+                context.tryStartActivity(Intent(Settings.ACTION_HOME_SETTINGS))
+            },
+            leadingIcon = {
+                Icon(imageVector = Icons.Rounded.HelpOutline, contentDescription = null)
+            }
+        )
+    }
 }
+
 
